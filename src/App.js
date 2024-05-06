6408f2b4b1cc48593e9a4500f5845cc47bce384b
Judithsusansoney <judithsusansoney>
2024-05-06 23:43:03 +0530

    created recipe add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AddRecipe from './components/AddRecipe';
+
+function App() {
+  return (
+    <div>
+      <AddRecipe/>
+      
+    </div>
+  );
+}
+
+export default App;
